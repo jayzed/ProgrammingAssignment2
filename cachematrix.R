7cbbb800aeb6b75a3560d773f99a82534311366a
Jordy Zagdanski <jordy@eventmobi.com>
2015-11-21 19:23:52 -0500

    Completed makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Makes a set list of functions that
+## 1. Set the matrix
+## 2. Get the matrix
+## 3. Set the inverse of the matrix
+## 4. Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## Sets inverse to null
+  inverse <- NULL
+  
+  ## 1. Set the matrix
+  set <- function(y) {
+    ## Sets y to replace x
+    x <<- y
+    ## Sets inverse to null again
+    inverse <<- NULL
+  }
+  
+  ## 2. Get the matrix
+  get <- function() x
+  
+  ## 3. Sets the inverse
+  setInverse <- function(newInverse) inverse <<- newInverse
+  
+  ## 4. Gets the inverse
+  getInverse <- function() inverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
